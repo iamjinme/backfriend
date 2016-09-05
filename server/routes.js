c56cb73192220cca3e38214fa41852a7a15516e1
Jinme Mirabal <mirabalj@gmail.com>
2016-09-05 13:51:09 -0400

    Add Bearer token to Basic Auth. Read user info from token. Save follow friend

--- server/routes.js
+++ server/routes.js
@@ -1,42 +1,71 @@
 import auth from 'basic-auth';
+import jwt from 'koa-jwt';
+import sha1 from 'sha1';
 import User from './models/user';
-import { baseApi } from './config';
+import { baseApi, secretKey } from './config';
 // Export a function that takes the router
 export default router => {
   // Set a prefix of our api, in this case api
   router.prefix('/' + baseApi);
 
   // POST Sign Up (register).
-  router.post('/signup', async (ctx, next) =>
-    ctx.body = await new User(ctx.request.body).save());
+  router.post('/signup', async (ctx, next) => {
+    var user = ctx.request.body;
+    user.password = sha1(user.password);
+    ctx.body = await new User(user).save();
+  });
 
   // GET Sign In (login).
   router.get('/signin', async(ctx) => {
+    var token = null;
+    // Basic Auth
     const credentials = auth(ctx);
-    if (!credentials || credentials.name !== 'john' || credentials.pass !== 'secret') {
+    // On credentials, find the user in database
+    if (credentials) {
+      const user = await User.findOne({ 'username': credentials.name, 'password': credentials.pass });
+      if (user) {
+        var profile = {
+          name: user.name,
+          username: user.username,
+          id: user._id,
+        };
+        // We are sending the profile inside the token
+        token = jwt.sign(profile, secretKey);
+      }
+    }
+    // Check login
+    if (!token) {
       ctx.status = 401
       ctx.response.set('WWW-Authenticate', 'Basic realm="backfriend"')
       ctx.body = { error: true, message: 'Access denied' };
     } else {
-      ctx.body = { success: true, message: 'Access granted' };
+      ctx.body = { success: true, message: 'Access granted', token: token };
     }
   });
 
-  // GET /api/user
+  // GET All users
   router.get('/user', async(ctx) =>
     ctx.body = await User.find());
 
-  // POST /api/user/:id/friends
-  router.post('/user/:id/friends', async(ctx) => {
-    const user = await User.findById(ctx.params.id);
+  // POST Follow friend
+  router.post('/user/:id/follow', async(ctx) => {
+    var token = ctx.headers.authorization.replace('Bearer ','');
+    var me = jwt.decode(token);
+    var friend = ctx.params.id;
+    const user = await User.findById(me.id);
     if (user) {
-      // TODO: Save friend
-      ctx.body = user;
+      var pos = user.friends.indexOf(friend);
+      if (pos < 0) {
+        user.friends.push(friend);
+        user.save();
+      }
+      ctx.body = { id: user._id, friend: friend, friends: user.friends.length };
     } else {
-      ctx.status = 401;
-      ctx.body = { error: true, message: 'Friend not found' };
+      ctx.status = 404;
+      ctx.body = { error: true, message: 'User not found' };
     }
   });
+
 
   return router;
 }
