929a295e3651d510d686bff66188e0f6c615ef9f
Jinme Mirabal <mirabalj@gmail.com>
2016-09-05 13:47:04 -0400

    Create Key Secret for Bearer Token

--- server/config.js
+++ server/config.js
@@ -1,3 +1,4 @@
 export const port = process.env.PORT || 3000
 export const mongodbUri = 'mongodb://localhost/backfriend'
 export const baseApi = 'api'
+export const secretKey = 'ThisIsASecretKeyForBackFriend'
