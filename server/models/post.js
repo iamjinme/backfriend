88effc8c5d6da5a53474efdcb3fb54026ec3afca
Jinme Mirabal <mirabalj@gmail.com>
2016-09-06 17:06:39 -0400

    Create model to Posts and comments

---
+++ server/models/post.js
@@ -0,0 +1,26 @@
+import mongoose from 'mongoose'
+
+const Schema = mongoose.Schema
+
+const Comments = new Schema({
+  author: {
+    id: ObjectId,
+    username: String
+  },
+  text: String,
+  date: Date
+});
+
+const Post = new Schema({
+	author: {
+    id: ObjectId,
+    username: String
+  },
+	title: String,
+	body: String,
+  image: String,
+	date: Date,
+  comments: [Comments]
+});
+
+export default mongoose.model('Post', Post)
