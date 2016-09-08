9e2b2fec5d8e2c525e107efe649b96b3c6864638
Jinme Mirabal <mirabalj@gmail.com>
2016-09-08 10:08:55 -0400

    Add Koa convert for deprecated syntax. Use Paginate middleware

--- server/index.js
+++ server/index.js
@@ -5,8 +5,11 @@ import mongoose from 'mongoose';
 import serve from 'koa-static';
 import Router from 'koa-router';
 import jwt from 'koa-jwt';
+// User convert to transform *deprecated syntax
+import convert from 'koa-convert';
 import { port, mongodbUri, secretKey } from './config'
 import routing from './routes';
+import paginate from './paginate';
 import Koa from 'koa';
 
 // Connect to Mongodb
@@ -19,24 +22,26 @@ const router = routing(Router());
 var app = new Koa();
 
 app
-	// Logger middleware
-  .use(logger())
-	// Parse JSON body request
-	.use(koaBody({ multipart: true, formidable:{ uploadDir: __dirname + '/uploads', multiples: false }}))
+	// Logger middleware *deprecated
+  .use(convert(logger()))
+	// Parse JSON body request *deprecated
+	.use(convert(koaBody({ multipart: true, formidable:{ uploadDir: __dirname + '/uploads', multiples: false }})))
+	// Paginate middleware
+	.use(paginate)
 	// Print ctx in any request
-	.use(async function(ctx, next) { 
+	.use(async function(ctx, next) {
     console.log(ctx);
     return await next();
   })
-	// Serve static files (images uploaded)
-	.use(serve(__dirname + '/uploads'))
+	// Serve static files (images uploaded) *deprecated
+	.use(convert(serve(__dirname + '/uploads')))
 	// JSON Web Tokens (Auth Bearer, except path urls)
-	.use(jwt({ secret: secretKey }).unless({ path: ['/api/signin', '/api/signup', '/', new RegExp('/uploads.*/', 'i')] }))
+	.use(convert(jwt({ secret: secretKey }).unless({ path: ['/api/signin', '/api/signup', '/', new RegExp('/uploads.*/', 'i')] })))
 	// Assigns routes
 	.use(router.routes())
 	.use(router.allowedMethods())
 
-	// Response
+	// Response in root
 	app.use(ctx => {
 	  ctx.body = 'RESTful API server running...';
 	});
